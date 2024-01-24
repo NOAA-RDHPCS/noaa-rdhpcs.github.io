da66991d1b202b04726b9458f750aa3573d76236
Seth Underwood <underwoo@underwoo.io>
2024-01-23 19:24:21 -0500

    Add javascript to add links to ...
    RDHPCS/HPCC home page, and to easily allow users to find the support page

--- source/conf.py
+++ source/conf.py
@@ -8,51 +8,53 @@
 
 import datetime as dt
 
-project = 'NOAA RDHPCS User Documentation'
-copyright = '%s, National Oceanic and Atmospheric Administration' % dt.datetime.now().year
-author = 'NOAA RDHPCS'
-html_logo = 'images/NOAA_RDHPCS.png'
-html_favicon = 'images/favicon.ico'
+project = "NOAA RDHPCS User Documentation"
+copyright = (
+    "%s, National Oceanic and Atmospheric Administration" % dt.datetime.now().year
+)
+author = "NOAA RDHPCS"
+html_logo = "images/NOAA_RDHPCS.png"
+html_favicon = "images/favicon.ico"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx_rtd_theme',
-    'sphinx_design',
-    'sphinxcontrib.mermaid'
-]
+extensions = ["sphinx_rtd_theme", "sphinx_design", "sphinxcontrib.mermaid"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_baseurl = 'https://docs.rdhpcs.noaa.gov'
+html_theme = "sphinx_rtd_theme"
+html_baseurl = "https://docs.rdhpcs.noaa.gov"
 
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 html_css_files = [
-    'css/theme_overrides.css',
+    "css/theme_overrides.css",
+]
+
+html_js_files = [
+    "js/custom.js",
 ]
 
 html_context = {
-    'vcs_pageview_mode': 'edit',
-    'display_github': True,
-    'github_user': 'NOAA-RDHPCS',  # Username
-    'github_repo': 'noaa-rdhpcs.github.io',  # Repo name
-    'github_version': 'main',  # Version
-    'conf_py_path': '/source/',  # Path in the checkout to the docs root
+    "vcs_pageview_mode": "edit",
+    "display_github": True,
+    "github_user": "NOAA-RDHPCS",  # Username
+    "github_repo": "noaa-rdhpcs.github.io",  # Repo name
+    "github_version": "main",  # Version
+    "conf_py_path": "/source/",  # Path in the checkout to the docs root
 }
 
 # see https://sphinx-rtd-theme.readthedocs.io/en/stable/configuring.html
 html_theme_options = {
-    'collapse_navigation': False,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'style_external_links': True,
-    'style_nav_header_background': '#efefef',
-    'logo_only': True,
+    "collapse_navigation": False,
+    "sticky_navigation": True,
+    "navigation_depth": 4,
+    "style_external_links": True,
+    "style_nav_header_background": "#efefef",
+    "logo_only": True,
 }
