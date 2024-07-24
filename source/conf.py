6531517c094d530dfe3bc2a93ef0052377b49c7d
Seth Underwood <Seth.Underwood@noaa.gov>
2024-07-24 16:23:06 -0400

    Add ability to add open issue button

--- source/conf.py
+++ source/conf.py
@@ -42,17 +42,20 @@ html_css_files = [
     "css/theme_overrides.css",
 ]
 
-html_js_files = [
-    "js/custom.js",
-]
+#html_js_files = [
+#    "js/custom.js",
+#]
 
 html_context = {
-    "vcs_pageview_mode": "edit",
     "display_github": True,
     "github_user": "NOAA-RDHPCS",  # Username
     "github_repo": "noaa-rdhpcs.github.io",  # Repo name
     "github_version": "main",  # Version
     "conf_py_path": "/source/",  # Path in the checkout to the docs root
+    "version": "<a href='/help/'>Need Help? Click Here</a>",
+    "project_home_url": "https://www.noaa.gov/information-technology/hpcc",
+    "project_name": False,
+    "github_issue_button": True,
 }
 
 # see https://sphinx-rtd-theme.readthedocs.io/en/stable/configuring.html
@@ -63,6 +66,9 @@ html_theme_options = {
     "style_external_links": True,
     "style_nav_header_background": "#efefef",
     "logo_only": True,
+    "display_version": True,
+    "vcs_pageview_mode": "blob",
+
 }
 
 linkcheck_allowed_redirects = {
